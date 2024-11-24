8ee949f1822cfd22dfce75a350c2223f3d5919f2
Megan Noel <144546725+megannoeln@users.noreply.github.com>
2024-11-24 10:28:33 -0500

    only show menu tab for vendor

--- TruckSpot/src/components/Sidebar/SideBar.jsx
+++ TruckSpot/src/components/Sidebar/SideBar.jsx
@@ -116,6 +116,7 @@ function SideBar() {
               <span className="ms-3">Dashboard</span>
             </Link>
           </li>
+          {isLoggedIn && userType === '1' && (
           <li>
             <Link to="/foodmenu" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
               <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
@@ -125,7 +126,7 @@ function SideBar() {
               <span className="ms-3">Menu</span>
             </Link>
           </li>
-          
+                )}
         </ul>
       </div>
     </aside>
