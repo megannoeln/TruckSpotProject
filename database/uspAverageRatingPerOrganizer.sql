2989b2802c0fe37c7394ba1c08d15d55e5dafb4d
seth <selubic@protonmail.com>
2024-11-27 00:01:09 -0500

    added procedure for averging all of an organizers ratings across all events

---
+++ database/uspAverageRatingPerOrganizer.sql
@@ -0,0 +1,40 @@
+-- --------------------------------------------------------------------------------
+-- Capstone Project Fall '24
+-- TruckSpot (FoodTruck & Event App)
+-- Team C
+-- Megan Noel, Seth Lubic, Testimony Awuzie & Apiwat Anachai
+--
+--
+-- --------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------
+-- Options
+-- --------------------------------------------------------------------------------
+USE truckspot;
+
+SET NOCOUNT ON;
+
+-- --------------------------------------------------------------------------------
+-- Drop
+-- --------------------------------------------------------------------------------
+
+IF OBJECT_ID('uspAverageRatingPerOrganizer') IS NOT NULL DROP PROCEDURE  uspAverageRatingPerOrganizer
+
+GO
+
+CREATE PROCEDURE uspAverageRatingPerOrganizer
+    @intOrganizerID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        AVG(intRating)
+    FROM
+        TFoodTruckEvents INNER JOIN TEvents
+    ON
+        TFoodTruckEvents.intEventID = TEvents.intEventID
+    WHERE
+        intOrganizerID = @intOrganizerID
+END;
+
+GO
